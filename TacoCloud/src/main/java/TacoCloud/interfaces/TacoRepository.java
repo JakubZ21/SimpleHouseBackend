0bcd64791309c62501b404c80e54e865949b8b11
JakubZ <jakub.zytkowski21@gmail.com>
2020-02-28 10:41:15 +0100

    Added Login page and Logout button visible only to those logged in
    Added Spring Security Support
        -Added Data Source Config
        -configured SS to be authenticated from DB
        -Added access denial page
    
    Restricted views from not logged users
        -restricted specific buttons like add meal button to not be visible for not logged in users
    
    Added bunch of TODOs in to keep track of what i have to do :)
    
    Added ability to create new categories, prepared buttons for Rename and Delete category (Not Supported Yet)

--- TacoCloud/src/main/java/TacoCloud/interfaces/TacoRepository.java
+++
@@ -1,8 +0,0 @@
-package TacoCloud.interfaces;
-
-import TacoCloud.Pojo.Taco;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TacoRepository extends CrudRepository<Taco, Long> {
-    Taco save(Taco design);
-}
